31046ed9f6a6c55de2fc37be000310f5cb880ede
Vinícius Silva Souza <164974674+ViniciusTheGreatest@users.noreply.github.com>
2025-12-04 23:52:17 -0300

    Add files via upload
    Este é o meu primeiro programa em C++ seguindo as instruções da autora do livro Treinamento em Linguagem C++: módulo 1 - Victorine VIVIANE Mizrahi

---
+++ Meu primeiro programa em C++.o.cpp
@@ -0,0 +1,13 @@
+//Este programa exibe uma mensagem de Primeiro Programa no terminal
+
+#include <iostream>
+
+int main () {
+
+using namespace std;
+
+    cout <<
+    "Primeiro Programa\n";
+    system("PAUSE");
+    return 0;
+}
