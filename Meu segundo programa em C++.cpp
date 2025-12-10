44cb53132c79ff9a7ac8b7eb394d17af443da803
Vinícius Silva Souza <164974674+ViniciusTheGreatest@users.noreply.github.com>
2025-12-10 16:59:17 -0300

    Add files via upload

---
+++ Meu segundo programa em C++.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+
+int main()
+{
+  int SegundosEm3Horas=3*60*60;
+  int _3MinutosEmSegundos=3*60;
+  int _5Minutos=5*60;// algum programador deixou um comentário acerca de 6 minutos, pegue-o(a)!
+  float valorPi=3.141526;
+
+  std::cout << "resultado:  " << SegundosEm3Horas <<  std::endl;
+  return 0;
+}
